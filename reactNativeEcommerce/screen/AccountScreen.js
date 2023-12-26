421108c391977ef44f77a41d73d9aae44223666d
nqTrong <nguyenquoctrongabkg@gmail.com>
2023-12-26 18:57:57 +0700

    second commit

--- reactNativeEcommerce/screen/AccountScreen.js
+++ reactNativeEcommerce/screen/AccountScreen.js
@@ -1,10 +1,69 @@
-
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 export default function AccountScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('https://fakestoreapi.com/auth/login', {
+        username: username,//mor_2314
+        password: password,//83r5^_
+      });
+      setToken(response.data.token);
+      setIsLoggedIn(true);
+    } catch (error) {
+      Alert.alert('Login failed', 'Check your username and password');
+    }
+  };
+
+  const userInfo = {
+    name: "Nguyễn Quốc Trọng",
+    email: "nqtrong@gmail.com",
+    address: "TP.HCM",
+  };
+
+  const handleLogout = () => {
+    setToken('');
+    setIsLoggedIn(false);
+  };
+
+  if (isLoggedIn) {
+    return (
+      <View style={styles.loggedInContainer}>
+        <Text style={[styles.welcomeText]}>Welcome, {userInfo.name}!</Text>
+        <Text style={styles.userInfoText}>Email: {userInfo.email}</Text>
+        <Text style={styles.userInfoText}>Address: {userInfo.address}</Text>
+        <Text style={[styles.tokenText]} numberOfLines={1} ellipsizeMode="tail">
+          Token: {token}
+        </Text>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.logoutButtonText}>LOGOUT</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View style={[styles.container]}>
-      <Text style={[styles.fontSizeText]}>Account Screen</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
@@ -12,12 +71,50 @@ export default function AccountScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    textAlign:'center',
     justifyContent: 'center',
-    backgroundColor: "#66c4e8",
+    backgroundColor: '#f0f4f7',
+    padding: 20,
   },
-  fontSizeText: {
-    fontSize: 15
-  }
+  loggedInContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f4f7',
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 10,
+    color: '#333',
+  },
+  userInfoText: {
+    fontSize: 18,
+    color: '#333',
+    margin: 5,
+  },
+  tokenText: {
+    fontSize: 16,
+    color: '#555',
+    margin: 10,
+    textAlign: 'center',
+  },
+  logoutButton: {
+    backgroundColor: '#de5246',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  logoutButtonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  input: {
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+  },
 });
--- reactNativeEcommerce/screen/CartScreen.js
+++ reactNativeEcommerce/screen/CartScreen.js
@@ -1,23 +1,146 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import axios from 'axios';
 
-import { StyleSheet, View, Text } from 'react-native';
+const fetchProductDetails = async (productId) => {
+  try {
+    const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+  }
+};
 
 export default function CartScreen() {
+  const [cartData, setCartData] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    getCartData();
+  }, []);
+
+  const getCartData = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/carts/user/2');
+      const cartItems = response.data[0]?.products || []; // Assuming we take the first cart for simplicity
+      const detailedCartItems = await Promise.all(
+        cartItems.map(async (item) => {
+          const productDetails = await fetchProductDetails(item.productId);
+          return { ...item, ...productDetails };
+        })
+      );
+      setCartData(detailedCartItems);
+      calculateTotal(detailedCartItems);
+    } catch (error) {
+      console.error('Error fetching cart:', error);
+    }
+  };
+
+  const calculateTotal = (items) => {
+    const total = items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+    setTotal(total);
+  };
+
+  const updateQuantity = (productId, increment = true) => {
+    const updatedCartData = cartData.map((item) => {
+      if (item.id === productId) {
+        const updatedQuantity = increment ? item.quantity + 1 : item.quantity - 1;
+        return { ...item, quantity: updatedQuantity >= 0 ? updatedQuantity : 0 };
+      }
+      return item;
+    });
+    setCartData(updatedCartData);
+    calculateTotal(updatedCartData);
+  };
+
+  const renderItem = ({ item, index }) => (
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: item.image }} style={styles.itemImage} />
+      <Text style={styles.itemTitle}>{item.title}</Text>
+      <Text style={styles.itemPrice}>${item.price}</Text>
+      <View style={styles.quantityContainer}>
+        <TouchableOpacity onPress={() => updateQuantity(item.id, false)} style={styles.quantityButton}>
+          <Text style={styles.quantityText}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.quantity}>{item.quantity}</Text>
+        <TouchableOpacity onPress={() => updateQuantity(item.id, true)} style={styles.quantityButton}>
+          <Text style={styles.quantityText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
   return (
-    <View style={[styles.container]}>
-      <Text style={[styles.fontSizeText]}>Cart Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={cartData}
+        keyExtractor={(item) => item?.id?.toString() || String(index)}
+        renderItem={renderItem}
+      />
+      <View style={styles.totalContainer}>
+        <Text style={styles.subTotalText}>Sub Total</Text>
+        <Text style={styles.totalText}>TOTAL: ${total.toFixed(2)}</Text>
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    textAlign:'center',
-    justifyContent: 'center',
-    backgroundColor: "#16b2e8",
+    backgroundColor: '#fff',
   },
-  fontSizeText: {
-    fontSize: 15
-  }
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  itemImage: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  itemTitle: {
+    flex: 1,
+  },
+  itemPrice: {
+    fontWeight: 'bold',
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantityButton: {
+    marginHorizontal: 10,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  quantityText: {
+    fontSize: 18,
+  },
+  quantity: {
+    fontSize: 18,
+  },
+  totalContainer: {
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+  },
+  subTotalText: {
+    textAlign: 'right',
+    fontSize: 18,
+    paddingVertical: 5,
+  },
+  totalText: {
+    textAlign: 'right',
+    fontSize: 22,
+    fontWeight: 'bold',
+    paddingVertical: 5,
+  },
 });
